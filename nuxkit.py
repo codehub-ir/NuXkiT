0afa0cd46bf24b8f092c2c4a8a1ec2e7ef1243c6
lnxpy <lnxpylnxpy@gmail.com>
2020-04-18 13:56:44 +0430

    intro interface shows well

--- nuxkit.py
+++ nuxkit.py
@@ -1,13 +1,30 @@
 from settings import *
 from os import system as sys
 from platform import system
+import requests
 
 
 cleaner = {'Windows': 'cls',
            'Linux': 'clear'}[system()]
 
 
-sys(cleaner)
-print(nux.format(
-                    login_user="----",
-                    network_status=internet_on(),))
+def connected_to_internet(url=server_url, timeout=5):
+
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return 'OK'
+    except requests.ConnectionError:
+        pass
+    return 'ERROR'
+
+
+def main(logged_in=False, username=''):
+
+    sys(cleaner)
+    print(nux.format(
+        login_user=username if logged_in else '----',
+        network_status=connected_to_internet(),))
+
+
+if __name__ == '__main__':
+    main()
